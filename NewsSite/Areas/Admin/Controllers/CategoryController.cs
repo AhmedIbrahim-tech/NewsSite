bae87a046e4d2b7e7f14d12fd87a56d502013e49
AhmedIbrahim-tech <ebrahema89859@gmail.com>
2022-06-25 15:36:26 +0200

    Add AdminLte Dashboard

---
+++ NewsSite/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NewsSite.Areas.Admin.Controllers
+{
+
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDBContext db;
+
+        public CategoryController(ApplicationDBContext db)
+        {
+            this.db = db;
+        }
+        public async Task<IActionResult> Index()
+        {
+            return View(await db.Categories.ToListAsync());
+        }
+    }
+}
---
+++ NewsSite/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NewsSite.Areas.Admin.Controllers
+{
+
+    [Area("Admin")]
+    public class HomeController : Controller
+    {
+        private readonly ApplicationDBContext db;
+
+        public HomeController(ApplicationDBContext db)
+        {
+            this.db = db;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
